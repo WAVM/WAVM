6b18947f50c920345f20e9a020cc47831198e817
Andrew Scheidecker <andrew@scheidecker.net>
2015-09-16 13:23:56 -0400

    Second try at getting Travis working...

--- travis/install-llvm.sh
+++ travis/install-llvm.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-echo "yes" | add-apt-repository 'deb http://llvm.org/apt/trusty/ llvm-toolchain-trusty-3.7 main'
+echo "yes" | sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main'
 
-wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | apt-key add -
-apt-get update
-apt-get install libllvm3.7 libllvm3.7-dbg llvm-3.7 llvm-3.7-dev llvm-3.7-runtime
+wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install libllvm3.7 libllvm3.7-dbg llvm-3.7 llvm-3.7-dev llvm-3.7-runtime
