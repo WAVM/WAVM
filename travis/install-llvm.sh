1009254789a164cc335f197018507e8eaf1aa73b
Andrew Scheidecker <andrew@scheidecker.net>
2015-09-16 14:01:08 -0400

    Try to fix install-llvm.sh error in Travis build: include ppa for libstdc++ backport

--- travis/install-llvm.sh
+++ travis/install-llvm.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+echo "yes" | sudo add-apt-repository 'deb http://ppa.launchpad.net/ubuntu-toolchain-r/test/ubuntu precise main'
 echo "yes" | sudo add-apt-repository 'deb http://llvm.org/apt/precise/ llvm-toolchain-precise-3.7 main'
 
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 1E9377A2BA9EF27F
 wget -O - http://llvm.org/apt/llvm-snapshot.gpg.key | sudo apt-key add -
+
 sudo apt-get update
-sudo apt-get install libllvm3.7 libllvm3.7-dbg llvm-3.7 llvm-3.7-dev llvm-3.7-runtime
+sudo apt-get install -y libllvm3.7 libllvm3.7-dbg llvm-3.7 llvm-3.7-dev llvm-3.7-runtime
