1e8aafab6a4993333fc99ee29f6326abd0625c8b
Andrew Scheidecker <andrew@scheidecker.net>
2015-10-09 09:37:49 -0400

    Reserve more stack space for handling stack overflow exceptions

--- Source/Runtime/RuntimeWindows.cpp
+++ Source/Runtime/RuntimeWindows.cpp
@@ -116,7 +116,7 @@ namespace RuntimePlatform
 	Value catchRuntimeExceptions(const std::function<Value()>& thunk)
 	{
 		// Ensure that there's enough space left on the stack in the case of a stack overflow to prepare the stack trace.
-		ULONG stackOverflowReserveBytes = 16384;
+		ULONG stackOverflowReserveBytes = 32768;
 		SetThreadStackGuarantee(&stackOverflowReserveBytes);
 
 		Exception* runtimeException = nullptr;
