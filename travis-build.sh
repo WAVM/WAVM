706dd69dcd69b4f94088d4d9b1ec89121f73a820
Andrew Scheidecker <andrew@scheidecker.net>
2016-03-15 20:48:14 -0400

    Enable Travis CI on OSX

--- travis-build.sh
+++ travis-build.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 
-set -e
+set -e -v
+
+if [[ $TRAVIS_OS_NAME == "osx" ]]; then
+  export CMAKE_LLVM_DIR_ARGUMENT="-DLLVM_DIR=$(brew --prefix llvm37)/lib/llvm-3.7/share/llvm/cmake";
+else
+  export CMAKE_LLVM_DIR_ARGUMENT="";
+fi
 
 mkdir release
 cd release
-cmake .. -DCMAKE_BUILD_TYPE=RELEASE
+cmake .. -DCMAKE_BUILD_TYPE=RELEASE $CMAKE_LLVM_DIR_ARGUMENT
 make
 ctest -V
 
 cd ..
 mkdir debug
 cd debug
-cmake .. -DCMAKE_BUILD_TYPE=DEBUG
+cmake .. -DCMAKE_BUILD_TYPE=DEBUG $CMAKE_LLVM_DIR_ARGUMENT
 make
