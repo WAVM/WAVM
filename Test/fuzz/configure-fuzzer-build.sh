cd14834da196c361d2bd2b19d7da141488f42bfe
Andrew Scheidecker <andrew@scheidecker.net>
2018-08-20 10:34:47 -0400

    Fix accidentally committed experiment in configure-fuzzer-build script

--- Test/fuzz/configure-fuzzer-build.sh
+++ Test/fuzz/configure-fuzzer-build.sh
@@ -8,19 +8,19 @@ WAVM_DIR=$(cd `dirname $0`/../.. && pwd)
 clang -v
 clang++ -v
 
-#rm -rf llvm7
-#mkdir llvm7
-#git clone -b release_70 http://github.com/llvm-mirror/llvm llvm7
+rm -rf llvm6
+mkdir llvm6
+git clone -b release_60 http://github.com/llvm-mirror/llvm llvm6
 
-#mkdir $BUILD_DIR/llvm7/build
-#cd $BUILD_DIR/llvm7/build
-#CC=clang CXX=clang++ cmake \
-#	-G Ninja \
-#	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
-#	-DLLVM_USE_SANITIZER=Address \
-#	-DLLVM_TARGETS_TO_BUILD=X86 \
-#	..
-#ninja
+mkdir $BUILD_DIR/llvm6/build
+cd $BUILD_DIR/llvm6/build
+CC=clang CXX=clang++ cmake \
+	-G Ninja \
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DLLVM_USE_SANITIZER=Address \
+	-DLLVM_TARGETS_TO_BUILD=X86 \
+	..
+ninja
 
 cd $BUILD_DIR
 CC=clang CXX=clang++ cmake \
@@ -28,6 +28,6 @@ CC=clang CXX=clang++ cmake \
 	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
 	-DENABLE_ASAN=1 \
 	-DENABLE_LIBFUZZER=1 \
-	-DLLVM_DIR=./llvm7/build/lib/cmake/llvm \
+	-DLLVM_DIR=./llvm6/build/lib/cmake/llvm \
 	$WAVM_DIR
 ninja
