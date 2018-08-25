f64a6d9b1804a7dc7900429d7cfb524b5961db02
Andrew Scheidecker <andrew@scheidecker.net>
2018-08-25 14:07:42 -0400

    Add FuzzAssemble fuzzer to complement FuzzDisassemble Parses the WAST input, then serializes it to WASM, then deserializes it and compares the deserialized module to the parsed module

---
+++ Test/fuzz/FuzzAssemble.cpp
@@ -0,0 +1,70 @@
+#include "IR/Module.h"
+#include "IR/Validate.h"
+#include "Inline/Assert.h"
+#include "Inline/BasicTypes.h"
+#include "Inline/CLI.h"
+#include "Inline/Serialization.h"
+#include "Logging/Logging.h"
+#include "ModuleMatcher.h"
+#include "WASM/WASM.h"
+#include "WAST/TestScript.h"
+#include "WAST/WAST.h"
+
+#include <string>
+
+using namespace WAST;
+using namespace IR;
+
+extern "C" I32 LLVMFuzzerTestOneInput(const U8* data, Uptr numBytes)
+{
+	// Copy the data to a std::vector and append a null terminator.
+	std::vector<U8> wastBytes(data, data + numBytes);
+	wastBytes.push_back(0);
+
+	Module wastModule;
+	std::vector<WAST::Error> parseErrors;
+	if(WAST::parseModule((const char*)wastBytes.data(), wastBytes.size(), wastModule, parseErrors))
+	{
+		std::vector<U8> wasmBytes;
+		try
+		{
+			Serialization::ArrayOutputStream stream;
+			WASM::serialize(stream, wastModule);
+			wasmBytes = stream.getBytes();
+		}
+		catch(Serialization::FatalSerializationException exception)
+		{
+			return 0;
+		}
+
+		Module wasmModule;
+		if(!loadBinaryModule(wasmBytes.data(), wasmBytes.size(), wasmModule))
+		{ Errors::fatal("Failed to deserialize the generated WASM file"); }
+
+		ModuleMatcher moduleMatcher(wastModule, wasmModule);
+		moduleMatcher.verify();
+	}
+
+	return 0;
+}
+
+#if !ENABLE_LIBFUZZER
+
+#include <cstring>
+
+I32 main(int argc, char** argv)
+{
+	if(argc != 2)
+	{
+		Log::printf(Log::error, "Usage: FuzzAssemble in.wast\n");
+		return EXIT_FAILURE;
+	}
+	const char* inputFilename = argv[1];
+
+	std::vector<U8> wastBytes;
+	if(!loadFile(inputFilename, wastBytes)) { return EXIT_FAILURE; }
+
+	LLVMFuzzerTestOneInput(wastBytes.data(), wastBytes.size());
+	return EXIT_SUCCESS;
+}
+#endif
---
+++ Test/fuzz/run-assemble-fuzzer.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -v
+
+BUILD_DIR=$(pwd)
+WAVM_DIR=$(cd `dirname $0`/../.. && pwd)
+
+cd $BUILD_DIR
+
+ninja
+
+mkdir assemble-corpus
+
+ASAN_OPTIONS=detect_leaks=0 bin/FuzzAssemble -use_value_profile=1 \
+  -workers=36 \
+  -jobs=36 \
+  -detect_leaks=0 \
+  assemble-corpus \
+    wast-corpus \
+    wast-seed-corpus
