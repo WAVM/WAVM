9dde6c2ffe72073aa083753c10aef63961e0bb50
Andrew Scheidecker <andrew@scheidecker.net>
2018-09-06 20:02:38 -0400

    Fix generate-seed-corpus script referencing removed Test/WAVM directory

--- Test/fuzz/generate-seed-corpus.sh
+++ Test/fuzz/generate-seed-corpus.sh
@@ -14,20 +14,12 @@ rm -rf wasm-seed-corpus
 
 mkdir wast-seed-corpus
 
-find $WAVM_DIR/Test/spec \
+find $WAVM_DIR/Test \
   -iname *.wast -not -iname skip-stack-guard-page.wast -not -iname br_table.wast \
-  | ASAN_OPTIONS=detect_leaks=0 xargs -n1 bin/DumpTestModules --wast --output-dir ./wast-seed-corpus 
-
-find $WAVM_DIR/Test/WAVM \
-  -iname *.wast \
   | ASAN_OPTIONS=detect_leaks=0 xargs -n1 bin/DumpTestModules --wast --output-dir ./wast-seed-corpus 
 
 mkdir wasm-seed-corpus
 
-find $WAVM_DIR/Test/spec \
+find $WAVM_DIR/Test \
   -iname *.wast -not -iname skip-stack-guard-page.wast -not -iname br_table.wast \
-  | ASAN_OPTIONS=detect_leaks=0 xargs -n1 bin/DumpTestModules --wasm --output-dir ./wasm-seed-corpus 
-
-find $WAVM_DIR/Test/WAVM \
-  -iname *.wast \
   | ASAN_OPTIONS=detect_leaks=0 xargs -n1 bin/DumpTestModules --wasm --output-dir ./wasm-seed-corpus 
