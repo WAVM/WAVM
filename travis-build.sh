8c6007ceffadfc88fc78ce6a26c76f4107c80857
Andrew Scheidecker <andrew@scheidecker.net>
2018-09-11 10:13:02 -0400

    Try using stock cmake on Travis

--- travis-build.sh
+++ travis-build.sh
@@ -13,25 +13,16 @@ if [ "$TRAVIS_OS_NAME" != "osx" ]; then
   fi
 fi
 
+echo $CXX
 $CXX --version
 
+cmake --version
+
 if [ $TRAVIS_OS_NAME == "osx" ]; then
-  export CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Darwin-x86_64.tar.gz";
   export LLVM_URL="http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-apple-darwin.tar.xz";
 else
-  export CMAKE_URL="https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz";
   export LLVM_URL="http://releases.llvm.org/6.0.0/clang+llvm-6.0.0-x86_64-linux-gnu-ubuntu-14.04.tar.xz";
 fi
-
-
-# Download a newer version of cmake than is available in Travis's whitelisted apt sources.
-mkdir cmake
-cd cmake
-wget --no-check-certificate --quiet -O ./cmake.tar.gz ${CMAKE_URL}
-tar --strip-components=1 -xzf ./cmake.tar.gz
-export PATH=`pwd`/bin:${PATH}
-cd ..
-cmake --version
 
 # Download a binary build of LLVM6 (also not available in Travis's whitelisted apt sources)
 mkdir llvm6
