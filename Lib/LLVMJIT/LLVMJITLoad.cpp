a7176a13aa91fde7ed215a4d2b1253178d6de5c1
Andrew Scheidecker <andrew@scheidecker.net>
2018-09-26 22:38:49 -0400

    Fix printf format specifier

--- Lib/LLVMJIT/LLVMJITLoad.cpp
+++ Lib/LLVMJIT/LLVMJITLoad.cpp
@@ -264,7 +264,7 @@ static void disassembleFunction(U8* bytes, Uptr numBytes)
 		nextByte += numInstructionBytes;
 
 		Log::printf(Log::output,
-					"\t\t0x%04x %s\n",
+					"\t\t0x%04" PRIxPTR " %s\n",
 					(nextByte - bytes - numInstructionBytes),
 					instructionBuffer);
 	};
