7d0739471f286d9f3377cf8abc7d1111e7062af3
Andrew Scheidecker <andrew@scheidecker.net>
2019-02-07 07:24:22 -0500

    Reduce the number of workers and increase the max memory for the instantiation fuzz target

--- Test/fuzz/run-instantiate-fuzzer.sh
+++ Test/fuzz/run-instantiate-fuzzer.sh
@@ -12,10 +12,10 @@ ninja
 mkdir instantiate-corpus
 
 ASAN_OPTIONS=detect_leaks=0 bin/FuzzInstantiate -use_value_profile=1 \
-  -workers=36 \
-  -jobs=36 \
+  -workers=18 \
+  -jobs=18 \
   -detect_leaks=0 \
-  -rss_limit_mb=2048 \
+  -rss_limit_mb=4096 \
   instantiate-corpus \
 	compile-corpus \
 	wasm-corpus \
