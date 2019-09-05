14bd30850f946346d581ca9cd90c14b130c6875c
Andrew Scheidecker <andrew@scheidecker.net>
2019-09-05 17:00:28 -0400

    Fix intrinsic macros omitting WAVM:: from some identifiers

--- Include/WAVM/Runtime/Intrinsics.h
+++ Include/WAVM/Runtime/Intrinsics.h
@@ -141,42 +141,52 @@ namespace WAVM { namespace Intrinsics {
 }}
 
 #define WAVM_DEFINE_INTRINSIC_MODULE(name)                                                         \
-	Intrinsics::Module* getIntrinsicModule_##name()                                                \
+	WAVM::Intrinsics::Module* getIntrinsicModule_##name()                                          \
 	{                                                                                              \
-		static Intrinsics::Module module;                                                          \
+		static WAVM::Intrinsics::Module module;                                                    \
 		return &module;                                                                            \
 	}
 
-#define WAVM_DECLARE_INTRINSIC_MODULE(name) extern Intrinsics::Module* getIntrinsicModule_##name();
+#define WAVM_DECLARE_INTRINSIC_MODULE(name)                                                        \
+	extern WAVM::Intrinsics::Module* getIntrinsicModule_##name();
 
 #define WAVM_INTRINSIC_MODULE_REF(name) getIntrinsicModule_##name()
 
 #define WAVM_DEFINE_INTRINSIC_FUNCTION(module, nameString, Result, cName, ...)                     \
-	static Result cName(Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__);           \
-	static Intrinsics::Function cName##Intrinsic(getIntrinsicModule_##module(),                    \
-												 nameString,                                       \
-												 (void*)&cName,                                    \
-												 Intrinsics::inferIntrinsicFunctionType(&cName),   \
-												 IR::CallingConvention::intrinsic);                \
-	static Result cName(Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__)
-
-#define WAVM_DEFINE_INTRINSIC_FUNCTION_WITH_CONTEXT_SWITCH(module, nameString, Result, cName, ...) \
-	static Intrinsics::ResultInContextRuntimeData<Result>* cName(                                  \
-		Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__);                           \
-	static Intrinsics::Function cName##Intrinsic(                                                  \
+	static Result cName(WAVM::Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__);     \
+	static WAVM::Intrinsics::Function cName##Intrinsic(                                            \
 		getIntrinsicModule_##module(),                                                             \
 		nameString,                                                                                \
 		(void*)&cName,                                                                             \
-		Intrinsics::inferIntrinsicWithContextSwitchFunctionType(&cName),                           \
-		IR::CallingConvention::intrinsicWithContextSwitch);                                        \
-	static Intrinsics::ResultInContextRuntimeData<Result>* cName(                                  \
-		Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__)
+		WAVM::Intrinsics::inferIntrinsicFunctionType(&cName),                                      \
+		WAVM::IR::CallingConvention::intrinsic);                                                   \
+	static Result cName(WAVM::Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__)
+
+#define WAVM_DEFINE_INTRINSIC_FUNCTION_WITH_CONTEXT_SWITCH(module, nameString, Result, cName, ...) \
+	static WAVM::Intrinsics::ResultInContextRuntimeData<Result>* cName(                            \
+		WAVM::Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__);                     \
+	static WAVM::Intrinsics::Function cName##Intrinsic(                                            \
+		getIntrinsicModule_##module(),                                                             \
+		nameString,                                                                                \
+		(void*)&cName,                                                                             \
+		WAVM::Intrinsics::inferIntrinsicWithContextSwitchFunctionType(&cName),                     \
+		WAVM::IR::CallingConvention::intrinsicWithContextSwitch);                                  \
+	static WAVM::Intrinsics::ResultInContextRuntimeData<Result>* cName(                            \
+		WAVM::Runtime::ContextRuntimeData* contextRuntimeData, ##__VA_ARGS__)
+
+#define WAVM_DEFINE_UNIMPLEMENTED_INTRINSIC_FUNCTION(module, nameString, Result, cName, ...)       \
+	WAVM_DEFINE_INTRINSIC_FUNCTION(module, nameString, Result, cName, __VA_ARGS__)                 \
+	{                                                                                              \
+		WAVM::Runtime::throwException(                                                             \
+			WAVM::Runtime::ExceptionTypes::calledUnimplementedIntrinsic);                          \
+	}
 
 // Macros for defining intrinsic globals, memories, and tables.
 #define WAVM_DEFINE_INTRINSIC_GLOBAL(module, name, Value, cName, initializer)                      \
-	static Intrinsics::GenericGlobal<Value> cName(getIntrinsicModule_##module(), name, initializer);
+	static WAVM::Intrinsics::GenericGlobal<Value> cName(                                           \
+		getIntrinsicModule_##module(), name, initializer);
 
 #define WAVM_DEFINE_INTRINSIC_MEMORY(module, cName, name, type)                                    \
-	static Intrinsics::Memory cName(getIntrinsicModule_##module(), #name, type);
+	static WAVM::Intrinsics::Memory cName(getIntrinsicModule_##module(), #name, type);
 #define WAVM_DEFINE_INTRINSIC_TABLE(module, cName, name, type)                                     \
-	static Intrinsics::Table cName(getIntrinsicModule_##module(), #name, type);
+	static WAVM::Intrinsics::Table cName(getIntrinsicModule_##module(), #name, type);
