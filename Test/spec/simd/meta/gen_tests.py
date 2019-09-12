315c5ecd694a1ff0e2bd6366862f480723c7d1a6
Wanming Lin <wanming.lin@intel.com>
2019-09-12 15:49:03 +0800

    [simd] Add spec tests for bitwise operations (#185)

--- Test/spec/simd/meta/gen_tests.py
+++ Test/spec/simd/meta/gen_tests.py
@@ -17,6 +17,7 @@ SUBMODULES = (
     'simd_i16x8_arith',
     'simd_i32x4_arith',
     'simd_f32x4_arith',
+    'simd_bitwise',
 )
 
 
@@ -28,6 +29,10 @@ def gen_group_tests(mod_name):
 
 
 def main():
+    """
+    Default program entry
+    """
+
     parser = argparse.ArgumentParser(
         description='Front-end script to call other modules to generate SIMD tests')
     parser.add_argument('-a', '--all', dest='gen_all', action='store_true',
