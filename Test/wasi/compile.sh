97890dcb13fade96f6e03ce1c7f3c6472bb4635e
Andrew Scheidecker <andrew@scheidecker.net>
2020-11-21 07:59:21 -0800

    Recompile WASI test binaries with latest SDK

--- Test/wasi/compile.sh
+++ Test/wasi/compile.sh
@@ -7,10 +7,10 @@ TEST_CPP=$2
 TEST_DIR=$(dirname $TEST_CPP)
 TEST_NAME=$(basename $TEST_CPP .cpp)
 
-$WASI_SDK_DIR/opt/wasi-sdk/bin/clang \
+$WASI_SDK_DIR/bin/clang \
   -O3 -g3 \
   -fdebug-prefix-map=$TEST_DIR=/ \
-  -fdebug-prefix-map=$WASI_SDK_DIR/opt/wasi-sdk=/wasi-sdk \
-  --sysroot $WASI_SDK_DIR/opt/wasi-sdk/share/wasi-sysroot/ \
+  -fdebug-prefix-map=$WASI_SDK_DIR=/wasi-sdk \
+  --sysroot $WASI_SDK_DIR/share/wasi-sysroot/ \
   -o $TEST_DIR/$TEST_NAME.wasm \
   $TEST_CPP
