7d865f618cbbb916240b116c64a335ce6ff43abc
Rochet2 <rochet2@post.com>
2020-11-21 16:59:47 +0200

    GC: Don't transfer roots from imported functions to the instances importing them (#294)

--- Lib/Runtime/ObjectGC.cpp
+++ Lib/Runtime/ObjectGC.cpp
@@ -194,7 +194,8 @@ static bool collectGarbageImpl(Compartment* compartment)
 			bool hasRootFunction = false;
 			for(Function* function : instance->functions)
 			{
-				if(function && function->mutableData->numRootReferences)
+				if(function && function->mutableData->numRootReferences
+				   && function->instanceId == instance->id)
 				{
 					hasRootFunction = true;
 					break;
