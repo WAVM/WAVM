b1d453e39be25a217bb5b5ea3e81a5128e567121
Piotr Sikora <piotrsikora@google.com>
2022-01-24 01:44:34 -0800

    Fix build with Clang 13.
    Fixes -Wunused-but-set-variable (enabled as part of -Wextra).
    
    Signed-off-by: Piotr Sikora <piotrsikora@google.com>

--- Lib/Platform/POSIX/ThreadPOSIX.cpp
+++ Lib/Platform/POSIX/ThreadPOSIX.cpp
@@ -173,6 +173,7 @@ WAVM_NO_ASAN static void touchStackPages(U8* minAddr, Uptr numBytesPerPage)
 		sum += *touchAddr;
 		if(touchAddr < minAddr + numBytesPerPage) { break; }
 	}
+	WAVM_SUPPRESS_UNUSED(sum);
 }
 
 bool Platform::initThreadAndGlobalSignalsOnce()
