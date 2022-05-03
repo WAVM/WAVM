21344d0160d73e3e79259ec92081e35e9dd0042a
Andrew Scheidecker <andrew@scheidecker.net>
2022-05-03 13:21:56 +0200

    Fix clang-format

--- Lib/Runtime/Table.cpp
+++ Lib/Runtime/Table.cpp
@@ -1,7 +1,7 @@
 #include <stdint.h>
 #include <string.h>
-#include <vector>
 #include <algorithm>
+#include <vector>
 #include "RuntimePrivate.h"
 #include "WAVM/IR/Types.h"
 #include "WAVM/Inline/Assert.h"
