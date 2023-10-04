ca2042dca331fbb9f702c75a1042b0e72b7f279d
小鸡快跑 <2283572185@qq.com>
2023-10-04 16:57:00 +0800

    link with new llvm

--- Lib/LLVMJIT/LLVMJITPrivate.h
+++ Lib/LLVMJIT/LLVMJITPrivate.h
@@ -163,7 +163,9 @@ namespace WAVM { namespace LLVMJIT {
 	{
 		llvm::Type** llvmTypes = (llvm::Type**)alloca(sizeof(llvm::Type*) * typeTuple.size());
 		for(Uptr typeIndex = 0; typeIndex < typeTuple.size(); ++typeIndex)
-		{ llvmTypes[typeIndex] = asLLVMType(llvmContext, typeTuple[typeIndex]); }
+		{
+			llvmTypes[typeIndex] = asLLVMType(llvmContext, typeTuple[typeIndex]);
+		}
 		return llvm::StructType::get(llvmContext,
 									 llvm::ArrayRef<llvm::Type*>(llvmTypes, typeTuple.size()));
 	}
@@ -220,7 +222,9 @@ namespace WAVM { namespace LLVMJIT {
 			numParameters = numImplicitParameters + functionType.params().size();
 			llvmArgTypes = (llvm::Type**)alloca(sizeof(llvm::Type*) * numParameters);
 			if(callingConvention != IR::CallingConvention::c)
-			{ llvmArgTypes[0] = llvmContext.i8PtrType; }
+			{
+				llvmArgTypes[0] = llvmContext.i8PtrType;
+			}
 
 			for(Uptr argIndex = 0; argIndex < functionType.params().size(); ++argIndex)
 			{
@@ -328,27 +332,27 @@ namespace WAVM { namespace LLVMJIT {
 			// WAVM should use once we can depend on it.
 			attrs =
 #if LLVM_VERSION_MAJOR > 13
-			attrs.addAttributeAtIndex
+				attrs.addAttributeAtIndex
 #else
-			attrs.addAttribute
+				attrs.addAttribute
 #endif
-			(function->getContext(),
-									   llvm::AttributeList::FunctionIndex,
-									   "no-frame-pointer-elim",
-									   "true");
+				(function->getContext(),
+				 llvm::AttributeList::FunctionIndex,
+				 "no-frame-pointer-elim",
+				 "true");
 
 			// Set the probe-stack attribute: this will cause functions that allocate more than a
 			// page of stack space to call the wavm_probe_stack function defined in POSIX.S
 			attrs =
 #if LLVM_VERSION_MAJOR > 13
-			attrs.addAttributeAtIndex
+				attrs.addAttributeAtIndex
 #else
-			attrs.addAttribute
+				attrs.addAttribute
 #endif
-			(function->getContext(),
-									   llvm::AttributeList::FunctionIndex,
-									   "probe-stack",
-									   "wavm_probe_stack");
+				(function->getContext(),
+				 llvm::AttributeList::FunctionIndex,
+				 "probe-stack",
+				 "wavm_probe_stack");
 
 			function->setAttributes(attrs);
 		}
@@ -444,4 +448,14 @@ namespace WAVM { namespace LLVMJIT {
 								 const llvm::object::SectionRef& xdataSection,
 								 const U8* xdataCopy,
 								 Uptr sehTrampolineAddress);
+
+// LLVM 13 requires some extra arguments in Create functions
+#define CreateLoad(Ptr) CreateLoad(Ptr->getType()->getScalarType()->getPointerElementType(), Ptr)
+#define CreateInBoundsGEP(Ptr, IdxList)                                                            \
+	CreateInBoundsGEP(Ptr->getType()->getScalarType()->getPointerElementType(), Ptr, IdxList)
+
+#define CreateAtomicCmpXchg(Op, Cmp, New, SuccessOrdering, FailureOrdering)                        \
+	CreateAtomicCmpXchg(Op, Cmp, New, llvm::MaybeAlign(), SuccessOrdering, FailureOrdering)
+#define CreateAtomicRMW(Op, Ptr, Val, Ordering)                                                    \
+	CreateAtomicRMW(Op, Ptr, Val, llvm::MaybeAlign(), Ordering)
 }}
