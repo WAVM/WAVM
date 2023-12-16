a9a9bb2db89c0b48e4571e59b9d5f6c86d73580f
trcrsired <uwgghhbcad@gmail.com>
2023-12-15 19:59:48 -0500

    add signatures

--- Include/WAVM/IR/OperatorSignatures.h
+++ Include/WAVM/IR/OperatorSignatures.h
@@ -361,7 +361,7 @@ namespace WAVM { namespace IR {
 				= asValueType(module_.memories.getType(imm.memoryIndex).indexType);
 			return OpSignature({}, {indexType, indexType});
 #else
-			return OpSignature({ValueType::i64},{ValueType::i64,ValueType::i64});
+			return OpSignature({},{ValueType::i64,ValueType::i64});
 #endif
 		}
 		inline OpSignature memory_randomstoretag(const Module& module_, const MemtagImm& imm)
