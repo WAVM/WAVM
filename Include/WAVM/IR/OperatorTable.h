21cd1a347e617bfec3dc8b154875edc0fad45272
trcrsired <uwgghhbcad@gmail.com>
2023-12-15 20:00:17 -0500

    operator table

--- Include/WAVM/IR/OperatorTable.h
+++ Include/WAVM/IR/OperatorTable.h
@@ -202,7 +202,7 @@
 	visitOp(0xfe4e, i64_atomic_rmw32_cmpxchg_u, "i64.atomic.rmw32.cmpxchg_u", AtomicLoadOrStoreImm<2>             , atomiccmpxchg_i64         , atomics                )   \
 /* Memtag                                                                                                                                                     		    */ \
 	visitOp(0xfc24, memory_storetag           , "memory.storetag"           , MemtagImm                           , memory_storetag           , memtag                 )   \
-	visitOp(0xfc28, memory_randomstoretag     , "memory.randomstoretag"           , MemtagImm  	             , memory_randomstoretag      , memtag                 )   \
+	visitOp(0xfc28, memory_randomstoretag     , "memory.randomstoretag"     , MemtagImm                           , memory_storetag           , memtag                 )   \
 
 #define WAVM_ENUM_NONCONTROL_NONPARAMETRIC_OPERATORS(visitOp)                                                                                                            \
 	visitOp(0x0001, nop                           , "nop"                           , NoImm                     , none_to_none              , mvp                    )   \
